ed050a4148ca4de7b7e082d470c67fb94c38ed62
jason8209067 <jason8209067@gmail.com>
2016-08-10 14:26:55 +0800

    add \

--- test0804.c
+++ test0804.c
@@ -15,13 +15,22 @@
 
 int main()
 {
-	int s;
+	int s,len;
 	struct sockaddr_in addr;
 	char buffer[1024];
-	int addr_len = sizeof(struct sockaddr_in);	
-
+	//int addr_len = sizeof(struct sockaddr_in);	
+	
+	char ap[]="GET /2.txt HTTP/1.1\r\n"
+		  "User-Agent:wget/1.15.1(linux-gnu)\r\n"
+  	          "Accept:*/*\r\n"
+	          "Accept-Encoding: identity\r\n"
+		  "Host: 129.168.128.61\r\n"
+		  "Connection: Keep-Alive\r\n\r\n";	 
 	
 
+
+
+FILE*fp;	
 	if((s=socket(AF_INET,SOCK_STREAM,0))<0){
 
 	perror("socket");
@@ -36,27 +45,29 @@ int main()
 	perror("connect");
 	exit(1);
 }	
-	sprintf(buffer,"GET /2.txt HTTP/1.1\r\n"
-	               "User-Agent:Wget/1.17.1(linux-gnu)\r\n"
-		       "Accept:*/*\r\n"
-		       "Accept-Encoding: identity\r\n"
-		       "Host : 192.168.128.95\r\n"
-		       "Connection: Keep-Alive\r\n\r\n"   );
+
 	
-	printf("%s\n",buffer);
+	sprintf(buffer,"%s",ap);
+	
+	//printf("%s\n",buffer);
 
 
 	write(s,buffer, sizeof(buffer));
 	//fp = fopen(buffer,"w");
 
 	bzero(buffer,sizeof(buffer));
-	while(1){
-//	recv(s,buffer,sizeof(buffer),0);
-//	printf("%s\n",buffer);
-//	bzero(buffer,sizeof(buffer));
 	recv(s,buffer,sizeof(buffer),0);
 	printf("%s\n",buffer);
-	}
+	fp = fopen("bb.txt","w");
+	while((len = recv(s,buffer,sizeof(buffer),0)) >0){
+	//recv(s,buffer,sizeof(buffer),0);
+	//printf("%s\n",buffer);
+	//bzero(buffer,sizeof(buffer));
+	fprintf(fp,"%s",buffer);
+	//recv(s,buffer,sizeof(buffer),0);
+	printf("%s",buffer);
+	bzero(buffer,sizeof(buffer));
+	}   
 	perror("send");	
+	exit(1);
 }
-
